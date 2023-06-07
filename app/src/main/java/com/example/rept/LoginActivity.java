9f1c8b9079a33974e29230d6675c70d73610b0c4
imjmjmim <imjmjmim0531@gmail.com>
2023-06-07 19:21:46 +0900

    캡챠 수정

--- app/src/main/java/com/example/rept/LoginActivity.java
+++ app/src/main/java/com/example/rept/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.rept;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +10,18 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.recaptcha.Recaptcha;
+import com.google.android.recaptcha.RecaptchaAction;
+import com.google.android.recaptcha.RecaptchaTasksClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,7 +29,10 @@ import org.json.JSONObject;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText et_id, et_pass;
-    private Button btn_login,btn_register;
+    private Button btn_login,btn_register,btn_captcha;
+
+    @Nullable private RecaptchaTasksClient recaptchaTasksClient = null;
+
 
     private void saveUserID(String userID) {
         // SharedPreferences에 저장하기
@@ -32,15 +43,33 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initializeRecaptchaClient();
 
         et_id = findViewById(R.id.et_id);
         et_pass = findViewById(R.id.et_pass);
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
+        btn_captcha = findViewById(R.id.btn_captcha);
+
+
+
+        btn_captcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (recaptchaTasksClient != null) {
+                    executeLoginAction();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Recaptcha 클라이언트가 초기화되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +78,8 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,4 +120,60 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+    private void initializeRecaptchaClient() {
+        Recaptcha
+                .getTasksClient(getApplication(), "6LcN3HYmAAAAACuKcpnkiNvirC-mCfBDTedaYg_2")
+                .addOnSuccessListener(
+                        this,
+                        new OnSuccessListener<RecaptchaTasksClient>() {
+                            @Override
+                            public void onSuccess(RecaptchaTasksClient client) {
+                                System.out.println("***************************************"+client);
+
+                                LoginActivity.this.recaptchaTasksClient = client;
+
+                            }
+                        })
+                .addOnFailureListener(
+                        this,
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                System.out.println("***************************************"+e);
+                                // Handle communication errors ...
+                                // See "Handle communication errors" section
+                            }
+                        });
+    }
+
+    private void executeLoginAction() {
+        if (recaptchaTasksClient != null) {
+            recaptchaTasksClient
+                    .executeTask(RecaptchaAction.LOGIN)
+                    .addOnSuccessListener(
+                            this,
+                            new OnSuccessListener<String>() {
+                                @Override
+                                public void onSuccess(String token) {
+                                    // Handle success ...
+                                    // See "What's next" section for instructions
+                                    // about handling tokens.
+                                    Toast.makeText(LoginActivity.this, "캡챠 인증이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                    .addOnFailureListener(
+                            this,
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Handle communication errors ...
+                                    // See "Handle communication errors" section
+                                    Toast.makeText(LoginActivity.this, "Recaptcha 실행에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+        } else {
+            Toast.makeText(LoginActivity.this, "Recaptcha 클라이언트가 초기화되지 않았습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
